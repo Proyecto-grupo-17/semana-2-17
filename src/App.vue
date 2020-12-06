dc089efbd34b34d5cd40387c3f95922ac878019f
Malvera <maavelasquezra@unal.edu.co>
2020-12-05 20:41:55 -0500

    first commit news

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
+
+    <div class="container-fluid">
+<!-- se llama a la seccion noticias -->
+          <page-noticias></page-noticias>
+      </div>
+     
+
   </div>
 </template>
 
 <script>
+// se importa el componente de noticias
+import PageNoticias from './components/PageNoticias.vue'
 
 export default {
   name: 'App',
+  components:{
+//se agrega el componente de noticias
+    PageNoticias
+  },
+  data(){
+
+  }
 }
 </script>
 
